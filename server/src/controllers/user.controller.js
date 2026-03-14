7040e51988a4b608d2df1ff07054961c1a403a88
Bedo Hossam <bedohossam404@gmail.com>
2026-03-14 02:38:46 +0200

    user.controller

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,75 @@
+import User from "../models/User.js";
+
+// Get logged-in user profile
+const getMyProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// Update user profile
+const updateMyProfile = async (req, res) => {
+  try {
+    const { name, age } = req.body;
+
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (name) user.name = name;
+    if (age) user.age = age;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      message: "Profile updated successfully",
+      user: {
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        age: updatedUser.age,
+        role: updatedUser.role
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// Deactivate account
+const deactivateAccount = async (req, res) => {
+  try {
+
+    const user = await User.findByIdAndDelete(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ message: "Account deleted successfully" });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export {
+  getMyProfile,
+  updateMyProfile,
+  deactivateAccount
+};
